3fccf112196907f23a3ad9836c18a27aeecf5a30
Goutham K <k.goutham2000@gmail.com>
2020-12-26 15:33:01 +0530

    chat screen set up to get data.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../widgets/chat/messages.dart';
+
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -27,32 +29,21 @@ class ChatScreen extends StatelessWidget {
               ),
             ],
             onChanged: (itemId) {
-              if(itemId == 'logout') {
+              if (itemId == 'logout') {
                 FirebaseAuth.instance.signOut();
               }
             },
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance // ignore: deprecated_member_use
-            .collection('chats/Es6kcu2Tji3PMLgT3FKL/messages')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.documents;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
             ),
-          );
-        },
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
