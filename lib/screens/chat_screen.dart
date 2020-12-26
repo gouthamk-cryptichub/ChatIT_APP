0398bbc031cccf37ee645dfc5b84fb613d22e3eb
Goutham K <k.goutham2000@gmail.com>
2020-12-26 13:45:41 +0530

    Auth completed!

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('ChatIT'),
+        actions: [
+          DropdownButton(
+            icon: Icon(Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemId) {
+              if(itemId == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          ),
+        ],
+      ),
       body: StreamBuilder(
         stream: Firestore.instance // ignore: deprecated_member_use
             .collection('chats/Es6kcu2Tji3PMLgT3FKL/messages')
